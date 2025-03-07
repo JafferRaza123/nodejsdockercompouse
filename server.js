0aabed25cd915dbab1ff2165f0221bdf3c09fc76
Syed Jaffer Raza Zaidi <133816888+JafferRaza123@users.noreply.github.com>
2025-03-07 20:29:57 +0500

    server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, World! Welcome to my Node.js App running on Docker Compose is still down.");
+  res.send("Hello, World! Welcome to my Node.js App running on Docker Compose is stil.");
 });
 
 app.listen(port, () => {
