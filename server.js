e52db7ffd394f4e0ebc90fcc21e7cb77621bf9c3
Syed Jaffer Raza Zaidi <133816888+JafferRaza123@users.noreply.github.com>
2025-03-07 20:22:15 +0500

    server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World! Welcome to my Node.js App running on Docker Compose is still down.");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+
